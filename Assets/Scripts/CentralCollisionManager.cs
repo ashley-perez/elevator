256d01c18ba4efd811913456361757007dbe57cd
ashley-perez <ashleyperez428@gmail.com>
2024-02-18 13:11:39 -0800

    cleaned up scripts

--- Assets/Scripts/CentralCollisionManager.cs
+++ Assets/Scripts/CentralCollisionManager.cs
@@ -7,17 +7,29 @@ using UnityEngine;
 public class CentralCollisionManager : MonoBehaviour
 {
 
-    public Vector3 door1OpenPosition;
-    public Vector3 door1ClosePosition;
-    public Vector3 door2OpenPosition;
-    public Vector3 door2ClosePosition;
+
     public Transform door1;
     public Transform door2;
     public float moveSpeed = 1f;
 
+    public float maxOpenDistance = 4f;
+
+    private Vector3 door1StartPosition;
+    private Vector3 door2StartPosition;
+
+
+    private Vector3 door1EndPosition;
+    private Vector3 door2EndPosition;
+
+    private bool doorsFullyOpened = false;
+
     private bool shouldOpenDoors = false;
     private bool shouldCloseDoors = false;
 
+    void Start() {
+        door1StartPosition = door1.position;
+        door2StartPosition = door2.position;
+    }
     void Update() {
         // check if doors should be moving
         if (shouldOpenDoors) {
@@ -34,12 +46,27 @@ public class CentralCollisionManager : MonoBehaviour
 
 
         if (openOrClose) {
-            door1.position += new Vector3(step, 0, 0);
-            door2.position -= new Vector3(step, 0, 0);
-        }
-        else if (!openOrClose) {
-            door1.position -= new Vector3(step, 0, 0);
-            door2.position += new Vector3(step, 0, 0);
+            if (Vector3.Distance(door1.position, door1StartPosition) < maxOpenDistance) {
+                door1.position += new Vector3(step, 0, 0);
+                door2.position -= new Vector3(step, 0, 0);
+            } else {
+                doorsFullyOpened = true;
+                shouldOpenDoors = false;
+                shouldCloseDoors = true;
+            }
+        } else if (!openOrClose && doorsFullyOpened) {
+            if (door1.position != door1StartPosition) {
+                door1.position = Vector3.MoveTowards(door1.position, door1StartPosition, step);
+            }
+            if (door2.position != door2StartPosition) {
+                door2.position = Vector3.MoveTowards(door2.position, door2StartPosition, step);
+            }
+
+            // reset variables
+            if (door1.position == door1StartPosition && door2.position == door2StartPosition) {
+                doorsFullyOpened = false; 
+                shouldCloseDoors = false;
+            }
         }
 
     }
